86041b23060eaad30c3de1ae038e199051aeff96
Olga <keo89@inbox.ru>
2021-09-14 08:24:35 +0300

    Тесты на DELETE

---
+++ tests/test_user_delete.py
@@ -0,0 +1,89 @@
+from lib.my_requests import MyRequests
+from lib.assertions import Assertions
+from lib.base_case import BaseCase
+
+
+class TestUserDelete(BaseCase):
+    def test_delete_user_2(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+
+        response2 = MyRequests.delete("/user/2",
+                                      headers={"x-csrf-token": self.token},
+                                      cookies={"auth_sid": self.auth_sid})
+
+        Assertions.assert_code_status(response2, 400)
+
+    def test_delete_authorise_user(self):
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        response3 = MyRequests.delete(f"/user/{user_id}",
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid})
+
+        Assertions.assert_code_status(response3, 200)
+
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+        )
+
+        Assertions.assert_code_status(response4, 404)
+        assert response4.content.decode("utf-8") == f"User not found", f"Unexpected content {response4.content}"
+
+    def test_delete_not_authorise_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+
+        register_data = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+
+        user_id = self.get_json_value(response2, "id")
+
+        response3 = MyRequests.delete(f"/user/{user_id}",
+                                      headers={"x-csrf-token": self.token},
+                                      cookies={"auth_sid": self.auth_sid})
+
+        Assertions.assert_code_status(response3, 400)
+
+
+
+
+
+
